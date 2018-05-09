af7e75e0c28ef5f4a65c21c5e7d645e9b27acbd0
LouiseHopfner <lhoepfner@gmail.com>
2018-05-09 10:07:16 +0200

    initial project setup

--- FeedbackApp/Startup.cs
+++ FeedbackApp/Startup.cs
@@ -11,24 +11,36 @@ namespace FeedbackApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+
+            } else
+            {
+                app.UseExceptionHandler("/Home/Error");
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles(); //Enables the use of static files
+
+            //Use Mvc
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+            //Handle status code errors
+            app.UseStatusCodePages("text/plain", "HTTP Error - Status Code: {0}");
+
         }
     }
 }
---
+++ FeedbackApp/gulpfile.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var _ = require('lodash'),
+    gulp = require('gulp');
+
+gulp.task('copy-assets', function () {
+    var assets = {
+        js: [
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './node_modules/bootstrap/dist/js/bootstrap.min.js',
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/jquery/dist/jquery.min.js'
+        ],
+        css: [
+            './node_modules/bootstrap/dist/css/bootstrap.css',
+            './node_modules/bootstrap/dist/css/bootstrap.min.css'
+        ]
+    };
+    _(assets).forEach(function (assets, type) {
+        gulp.src(assets).pipe(gulp.dest('./wwwroot/' + type));
+    });
+});
